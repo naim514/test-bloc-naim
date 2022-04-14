48b00bb8c3022c910abbcaeb61a3e83b0ff316ae
naim <96814060+naim514@users.noreply.github.com>
2022-04-15 01:21:10 +0200

    Mise à jour custom.ts

--- custom.ts
+++ custom.ts
@@ -6,23 +6,11 @@
  */
 //% weight=80 color=#0fbc11 icon=""
 namespace animations {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block="blink x $x $y every $interval  ms"
+    
+    //% block="blink x $x y $y every $interval  ms"
     export function blink(x: number, y: number, interval: number): void {
         // Add code here
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
+    
 }
