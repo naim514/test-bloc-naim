0212b223b4b6f33702e37a05ed939fc30b2a669d
naim <96814060+naim514@users.noreply.github.com>
2022-04-15 01:39:03 +0200

    Mise à jour custom.ts

--- custom.ts
+++ custom.ts
@@ -5,7 +5,7 @@
  * animations blocks
  */
 //% weight=80 color=#0fbc11 icon=""
-namespace animations {
+namespace animation_naim {
     
     //% block="blink x $x y $y every $interval  ms"
     export function blink(x: number, y: number, interval: number): void {
